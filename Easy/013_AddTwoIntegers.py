f956c982af0b0b3dde7270c107a11e7098702c1d
Sonu Shahuji <sonushahuji4@gmail.com>
2024-10-14 18:54:51 +0530

    Create 013_AddTwoIntegers.py

---
+++ Easy/013_AddTwoIntegers.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/add-two-integers/description/?envType=company&envId=google&favoriteSlug=google-three-months&difficulty=EASY&role=full-stack
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
+        
