18fa53baf79fe493169cc64ffa52e76cfaad621a
Sonu Shahuji <sonushahuji4@gmail.com>
2024-10-15 22:36:12 +0530

    Update 001_TwoSum.py

--- Easy/001_TwoSum.py
+++ Easy/001_TwoSum.py
@@ -5,24 +5,24 @@ class Solution:
 
         # Brute force solution O(n^2)
         
-        # n = len(nums)
-        # for i in range(n):
-        #     for j in range(i+1,n):
-        #         if nums[i] + nums[j] == target: return [i,j]
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i] + nums[j] == target: return [i,j]
         
         # Better solution O(nlog) (This solution will not work since index matter here, sorting changes the index when you return)
         
-        # nums.sort()
-        # n = len(nums)
-        # leftPointer = 0
-        # rightPointer = n-1
-        # while leftPointer < rightPointer:
-        #     total = nums[leftPointer] + nums[rightPointer]
-        #     if total == target: return [leftPointer, rightPointer]
-        #     elif total < target:
-        #         leftPointer += 1
-        #     else:
-        #         rightPointer -= 1
+        nums.sort()
+        n = len(nums)
+        leftPointer = 0
+        rightPointer = n-1
+        while leftPointer < rightPointer:
+            total = nums[leftPointer] + nums[rightPointer]
+            if total == target: return [leftPointer, rightPointer]
+            elif total < target:
+                leftPointer += 1
+            else:
+                rightPointer -= 1
 
         # Optimal Solution O(n)
 
