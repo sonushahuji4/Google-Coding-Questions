d30cf95fd8ac4c086db8d229aa4670027bae0725
Sonu Shahuji <sonushahuji4@gmail.com>
2024-10-17 13:26:33 +0530

    Update 004_Sort Integers by The Power Value.py

--- Medium/004_Sort Integers by The Power Value.py
+++ Medium/004_Sort Integers by The Power Value.py
@@ -4,20 +4,45 @@ import heapq
 
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
-
+        
+        # Approach One
         def getPowerOfX(x):
-            if x % 2 == 0:
-                return x//2
+            steps = 0
+            while x != 1:
+                if x % 2 == 0:
+                    x = x//2
+                else:
+                    x = (3*x+1)
+                steps += 1
+            return steps
+                
+        
+        minHeap = []
+        while lo <= hi:
+            count = getPowerOfX(lo)
+            heapq.heappush(minHeap,(count,lo))
+            lo += 1
+        
+        num = steps = 0
+        while k > 0:
+            steps, num = heapq.heappop(minHeap)
+            k -=1
+        return num
+
+        # Approach Two
+        dp = dict()
+        def getPowerOfX(x):
+            if x in dp: return dp[x]
+            if x == 1: return 0
+            if x%2 == 0:
+                dp[x] = 1 + getPowerOfX(x//2)
             else:
-                return (3*x+1)
+                dp[x] = 1 + getPowerOfX(3 * x +1)
+            return dp[x]
 
         minHeap = []
         while lo <= hi:
-            num = lo
-            count = 0
-            while num != 1:
-                count += 1
-                num = getPowerOfX(num)
+            count = getPowerOfX(lo)
             heapq.heappush(minHeap,(count,lo))
             lo += 1
         
