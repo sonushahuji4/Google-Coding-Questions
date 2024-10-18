d584317c9e2a69334e70e94b1c5ba77859e955d4
Sonu Shahuji <sonushahuji4@gmail.com>
2024-10-18 10:15:20 +0530

    Update 008_Longest Increasing Subsequence.py

--- Medium/008_Longest Increasing Subsequence.py
+++ Medium/008_Longest Increasing Subsequence.py
@@ -58,6 +58,21 @@ class Solution:
         return dp[0][0]
 
 
+    # Approach Four
+    class Solution:
+        def lengthOfLIS(self, nums: List[int]) -> int:
+            n = len(nums)
+            if n == 0: return 0
+    
+            dp = [1] * n
+            for i in range(1,n):
+                for j in range(i):
+                    if nums[i] > nums[j]:
+                        dp[i] = max(dp[i], dp[j] + 1)
+    
+            return max(dp)
+
+
 
     # Approach One (Recursion) Variation
         def lis(ind, prev, n):
